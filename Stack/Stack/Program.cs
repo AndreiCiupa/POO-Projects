6afd4c93bbe58c9c82b99632221c0be42a62ae68
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2020-05-27 23:56:40 +0300

    Update Program.cs

--- Stack/Stack/Program.cs
+++ Stack/Stack/Program.cs
@@ -11,12 +11,25 @@ namespace Stiva
     {
         static void Main(string[] args)
         {
+            Console.Write("Introduceti un numar: ");
             int n = int.Parse(Console.ReadLine());
+
             mystack<int> stiva = new mystack<int>(n);
 
             stiva.Push(567);
             stiva.Push(568);
             stiva.Push(569);
+            stiva.Pop();
+            stiva.Clear();
+
+            Console.Write("\nIntroduceti un numar: ");
+            int m = int.Parse(Console.ReadLine());
+
+            mystack<char> stiva2 = new mystack<char>(n);
+
+            stiva.Push('a');
+            stiva.Push('b');
+            stiva.Push('c');
             stiva.Pop();
             stiva.Clear();
         }
