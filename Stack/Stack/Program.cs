da281f8f3445ccccfea6cd07947a7762ed07ee12
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2020-05-27 16:16:42 +0300

    Add files via upload

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            stack stiva = new stack();
+
+            stiva.Push();
+            stiva.Pop();
+            stiva.Clear();
+        }
+    }
+}
