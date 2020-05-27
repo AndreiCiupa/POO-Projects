a7be4ccaf663d26dda1a5c908c7ca85b1de52788
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2020-05-27 23:20:22 +0300

    Add files via upload

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stack;
+
+namespace Stiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            mystack<int> stiva = new mystack<int>(n);
+
+            stiva.Push(567);
+            stiva.Push(568);
+            stiva.Push(569);
+            stiva.Pop();
+            stiva.Clear();
+        }
+    }
+
+}
---
+++ Stack/Stack/stack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Stack
+{
+    class mystack<s>
+    {
+        int capacitate;
+        s[] stiva;
+        int top = -1;
+
+        public mystack(int n)
+        {
+            capacitate = n;
+            stiva = new s[capacitate];
+        }
+
+        public void Push(s Element)
+        {
+            // Verifica daca Stiva este plina
+            if (top == capacitate)
+            {
+                Console.WriteLine("Stiva este plina!");
+            }
+            else
+            {
+                // Adauga element in stiva
+                top = top + 1;
+                stiva[top] = Element;
+                Console.WriteLine($"Elementul {Element} introdus in Stiva!");
+            }           
+        }
+
+        public void Pop()
+        {
+            s ElementSters;
+
+            if (!(top <= 0))
+            {
+                ElementSters = stiva[top];
+                top = top - 1;
+                Console.WriteLine($"Elementul {ElementSters} a fost sters din Stiva!");
+            }
+           else Console.WriteLine("Stiva este goala!");
+        }
+
+        public void Clear()
+        {
+            s ElementSters;
+
+            for (int i=0; i < capacitate; i++)
+            {
+                if (!(top <= 0))
+                {
+                    ElementSters = stiva[top];
+                    top = top - 1;
+                }
+            }
+            Console.WriteLine("Stiva este goala!");
+        }
+    }
+}
